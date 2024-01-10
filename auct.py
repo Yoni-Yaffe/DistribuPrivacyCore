7930e6645269d6214e3a457357bf6aded478f036
TomK-TAU <tomkaninin@mail.tau.ac.il>
2024-01-10 12:34:14 +0200

    Implement naive and auction algorithms

---
+++ auct.py
@@ -0,0 +1,83 @@
+import numpy as np
+
+
+def auction_assignment(
+    biadjecancy_matrix: np.ndarray, minimize=True, scaling=True, eps=0.0, theta=0.15
+):
+    switch_assign = False
+    if biadjecancy_matrix.shape[0] > biadjecancy_matrix.shape[1]:
+        biadjecancy_matrix = biadjecancy_matrix.transpose()
+        switch_assign = True
+
+    if minimize:
+        biadjecancy_matrix = biadjecancy_matrix.max() - biadjecancy_matrix
+
+    if scaling:
+        assignments = auction_with_scaling(biadjecancy_matrix, theta)
+    else:
+        assignments = auction(biadjecancy_matrix, eps)
+
+    if switch_assign:
+        assignments[:, 0], assignments[:, 1] = (
+            assignments[:, 1],
+            assignments[:, 0].copy(),
+        )
+
+    return assignments
+
+
+def auction(biadjecancy_matrix: np.ndarray, eps):
+    unhappy_bidders = list(range(biadjecancy_matrix.shape[0]))
+    prices = np.zeros(biadjecancy_matrix.shape[1])
+    assignment = np.arange(biadjecancy_matrix.shape[0])
+
+    while len(unhappy_bidders) > 0:
+        bidder = unhappy_bidders.pop(0)
+        rel_vals = (biadjecancy_matrix[bidder] - prices).flatten()
+
+        cur_val_obj = assignment[bidder]
+        cur_val = rel_vals[cur_val_obj]
+
+        max_val_obj = np.argmax(rel_vals)
+        max_val = rel_vals[max_val_obj]
+
+        if max_val > cur_val + eps:
+            outbidded = np.argwhere(assignment == max_val_obj)
+            assignment[bidder], assignment[outbidded] = (
+                assignment[outbidded],
+                assignment[bidder],
+            )
+            unhappy_bidders.append(outbidded)
+            prices[max_val_obj] += max_val - np.partition(rel_vals, -2)[-2] + eps
+
+    return np.array(list(enumerate(assignment)))
+
+
+def auction_with_scaling(biadjecancy_matrix: np.ndarray, theta):
+    prices = np.zeros(biadjecancy_matrix.shape[1])
+    assignment = np.arange(biadjecancy_matrix.shape[0])
+    eps = biadjecancy_matrix.max() / 2
+
+    while eps > theta / biadjecancy_matrix.shape[0]:
+        unhappy_bidders = list(range(biadjecancy_matrix.shape[0]))
+        while len(unhappy_bidders) > 0:
+            bidder = unhappy_bidders.pop(0)
+            rel_vals = (biadjecancy_matrix[bidder] - prices).flatten()
+
+            cur_val_obj = assignment[bidder]
+            cur_val = rel_vals[cur_val_obj]
+
+            max_val_obj = np.argmax(rel_vals)
+            max_val = rel_vals[max_val_obj]
+
+            if max_val > cur_val + eps:
+                outbidded = np.argwhere(assignment == max_val_obj)
+                assignment[bidder], assignment[outbidded] = (
+                    assignment[outbidded],
+                    assignment[bidder],
+                )
+                unhappy_bidders.append(outbidded)
+                prices[max_val_obj] += max_val - np.partition(rel_vals, -2)[-2] + eps
+        eps *= theta
+
+    return np.array(list(enumerate(assignment)))
--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import numpy as np
 from scipy.spatial.distance import cdist
 from scipy import sparse
-from networkx.algorithms.bipartite import from_biadjacency_matrix, minimum_weight_full_matching
+from networkx.algorithms.bipartite import (
+    from_biadjacency_matrix,
+    minimum_weight_full_matching,
+)
+import auct
+import matplotlib.pyplot as plt
 # import dlib
 
 
-N = 5
+N = 128
 GRID = 0, 1024
+EXPERIMENTS_NUM = 5
+STD_SCALE = np.linspace(0, np.sqrt(GRID[1]))
 
 
 def polar_to_cartesian(r, theta):
     return np.stack([r * np.cos(theta), r * np.sin(theta)], 1)
+
 
 def get_matching_from_biadjecncy_matrix(biadjecancy_matrix: np.array):
     distance_scipy = sparse.csr_matrix(biadjecancy_matrix)
@@ -20,21 +28,78 @@ def get_matching_from_biadjecncy_matrix(biadjecancy_matrix: np.array):
     matching_array[:, 1] = matching_array[:, 1] - N
     return matching_array
 
+
+def naive_assignment(biadjecancy_matrix: np.array, minimize=True):
+    switch_assign = False
+    if biadjecancy_matrix.shape[0] > biadjecancy_matrix.shape[1]:
+        biadjecancy_matrix = biadjecancy_matrix.transpose()
+        switch_assign = True
+
+    if minimize:
+        biadjecancy_matrix = biadjecancy_matrix.max() - biadjecancy_matrix
+
+    dtype = [("ind", int), ("val", int)]
+    assignments = np.full(biadjecancy_matrix.shape[0], -1, int)
+    for obj in range(biadjecancy_matrix.shape[0]):
+        not_taken = np.setdiff1d(
+            np.arange(biadjecancy_matrix.shape[1]), assignments, True
+        )
+        assignment = np.partition(
+            np.array(list(enumerate(biadjecancy_matrix[obj])), dtype=dtype)[not_taken],
+            -1,
+            order="val",
+        )[-1][0]
+        assignments[obj] = assignment
+
+    if switch_assign:
+        assignments[:, 0], assignments[:, 1] = (
+            assignments[:, 1],
+            assignments[:, 0].copy(),
+        )
+
+    return np.array(list(enumerate(assignments)))
+
+
 def main():
     np.random.seed(0)
-    drivers, passengers = np.rollaxis(np.random.uniform(*GRID, (2, N, 2)), 0)
-    distances = cdist(drivers, passengers)
 
-    noise = np.abs(np.random.normal(0, np.sqrt(GRID[1]), N)), np.random.uniform(0, 2 * np.pi, N)
-    fake_passengers = passengers + polar_to_cartesian(*noise)
-    noise_distances = cdist(drivers, fake_passengers)
+    apx_errors_avg = list()
+    apx_errors_std = list()
+    for std in STD_SCALE:
+        apx_errors = list()
+        for experiment in range(EXPERIMENTS_NUM):
+            drivers, passengers = np.rollaxis(np.random.uniform(*GRID, (2, N, 2)), 0)
+            distances = cdist(drivers, passengers)
 
-    matching_array_without_noise = get_matching_from_biadjecncy_matrix(distances)
-    matching_array_with_noise = get_matching_from_biadjecncy_matrix(noise_distances)
-    dist_without_noise = distances[tuple(np.transpose(matching_array_without_noise))].sum()
-    dist_with_noise = distances[tuple(np.transpose(matching_array_with_noise))].sum()
-    print(dist_without_noise - dist_with_noise)
-    x = 1
+            noise = np.random.normal(0, std, N), np.random.uniform(0, np.pi, N)
+            fake_passengers = passengers + polar_to_cartesian(*noise)
+            noise_distances = cdist(drivers, fake_passengers)
+
+            matching_array_without_noise = get_matching_from_biadjecncy_matrix(
+                distances
+            )
+            matching_array_with_noise = naive_assignment(distances)
+
+            dist_without_noise = distances[
+                tuple(np.transpose(matching_array_without_noise))
+            ].sum()
+            dist_with_noise = distances[
+                tuple(np.transpose(matching_array_with_noise))
+            ].sum()
+            apx_errors.append(
+                np.abs(dist_without_noise - dist_with_noise) / dist_without_noise
+            )
+        apx_errors_avg.append(np.average(apx_errors))
+        apx_errors_std.append(np.std(apx_errors))
+
+    plt.errorbar(
+        STD_SCALE, apx_errors_avg, yerr=apx_errors_std, label="hungarian on noise"
+    )
+
+    plt.xlabel("Std of noise")
+    plt.ylabel("Approximation error of solution on noised problem")
+
+    plt.show()
 
 
 if __name__ == "__main__":
